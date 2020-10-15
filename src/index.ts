d409537c70bad0ca4dae202924f6cdf63f632dfe
lvjiawen <lvjiawen@zhongan.com>
2020-10-15 12:00:44 +0800

    fix some error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { useLayoutEffect, useCallback, useRef } from 'react';
 
-export default function useEventCallback<T extends ((...args: any[]) => any)>(fn: T) {
+export function useEventCallback<T extends ((...args: any[]) => any)>(fn: T) {
   const ref = useRef<T>(fn);
   useLayoutEffect(() => {
     ref.current = fn;
